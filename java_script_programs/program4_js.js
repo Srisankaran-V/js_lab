16ce1122dd6c38c906b64a3f274be2ad3cd39e29
Srisankaran-V <111955978+Srisankaran-V@users.noreply.github.com>
2023-04-25 16:03:53 +0530

    Add files via upload

---
+++ java_script_programs/program4_js.js
@@ -0,0 +1,32 @@
+const pizza = document.getElementById("pizza")
+const toppings = document.getElementById("toppings")
+const deepPan = document.getElementById("deeppan")
+const homeDelivery = document.getElementById("homedelivery")
+const bill = document.getElementById("bill")
+let pizzaCost = 0
+let toppingsCost = 0
+let totalCost = 0
+
+pizza.addEventListener("change", (e) => {
+    pizzaCost = parseInt(e.target.value)
+    console.log(pizzaCost)
+})
+
+toppings.addEventListener("change", (e) => {
+    toppingsCost = parseInt(e.target.value)
+    console.log(toppingsCost)
+})
+
+function calculateBill() {
+    totalCost += pizzaCost
+    totalCost += toppingsCost
+    if (deepPan.checked) totalCost += 20
+    if (homeDelivery.checked) totalCost += 25
+    return totalCost
+}
+
+bill.addEventListener("click", () => {
+    
+    document.getElementById("total_val").innerText="Your Total Amount is "+calculateBill();
+    totalCost = 0
+})
